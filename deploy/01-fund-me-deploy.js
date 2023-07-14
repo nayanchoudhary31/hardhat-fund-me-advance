c92a756961306c4d79b41d845897ac519102a0da
nayanchoudhary31 <nayanscsit09@gmail.com>
2023-07-14 16:39:32 +0530

    Updated Code

--- deploy/01-fund-me-deploy.js
+++ deploy/01-fund-me-deploy.js
@@ -1,14 +1,46 @@
 const { network } = require("hardhat");
-const { networkConfig } = require("../hardhat-helper-config");
+const {
+  networkConfig,
+  developmentChains,
+} = require("../hardhat-helper-config");
+const { verify } = require("../utils/verify");
 
 // Hardhat automatically give hre when we run using hardhat-deploy
 module.exports = async (hre) => {
   const { getNamedAccounts, deployments } = hre;
   const { deploy, log } = deployments;
-  const { deployer } = await getNamedAccounts;
+  const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
 
-  const ethUSDPriceFeed = networkConfig[chainId]["ethUSDPriceFeed"];
+  let ethUSDPriceFeed;
+  // const ethUSDPriceFeed = networkConfig[chainId]["ethUSDPriceFeed"];
 
   // When we use localhost or hardhat we need to use mocks
+  if (chainId === 31337) {
+    // Get the Mock Aggregator Address
+    const ethUSDMockAggregator = await deployments.get("MockV3Aggregator");
+    ethUSDPriceFeed = ethUSDMockAggregator.address;
+    console.log(
+      "🚀 ~ file: 01-fund-me-deploy.js:22 ~ module.exports= ~ ethUSDPriceFeed:",
+      ethUSDPriceFeed
+    );
+  } else {
+    ethUSDPriceFeed = networkConfig[chainId]["ethUSDPriceFeed"];
+  }
+
+  const fundMe = await deploy("FundMe", {
+    from: deployer,
+    args: [ethUSDPriceFeed],
+    log: true,
+    // we need to wait if on a live network so we can verify properly
+    waitConfirmations: network.config.blockConfirmations || 1,
+  });
+
+  log("----------------------------------------------------------------");
+
+  if (chainId !== 31337) {
+    await verify(fundMe.address, [ethUSDPriceFeed]);
+  }
+
+  module.exports.tags = ["all", "fundme"];
 };
--- hardhat.config.js
+++ hardhat.config.js
@@ -6,16 +6,6 @@ require("hardhat-gas-reporter");
 require("hardhat-deploy");
 require("solidity-coverage");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -25,18 +15,29 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.7",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    mumbai: {
+      url: process.env.MUMBAI_MATIC_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 80001,
+      blockConfirmations: 6,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6,
     },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
+    coinmarketcap: process.env.COINCAPMARKET_API_KEY,
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY,
+      polygonMumbai: process.env.POLYGON_API_KEY,
+    },
   },
   namedAccounts: {
     deployer: {
